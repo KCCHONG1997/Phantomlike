f9409caaf4ec5f0c8a912e0663ab299e20eb184c
KCCHONG1997 <94148987+KCCHONG1997@users.noreply.github.com>
2024-12-20 02:34:55 +0800

    rollback to stable version

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,13 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./view/Home";
-import DevilordPage from "./view/DevilordPage";
 
 const App: React.FC = () => {
-  const isCursed = localStorage.getItem("Cursed") === "true";
-
   return (
-    <Router basename="/Phantomlike">
-      <div style={{ marginTop: "0", padding: "20px", backgroundColor: "black" }}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          {!isCursed && <Route path="/devilord" element={<DevilordPage />} />}
-        </Routes>
+    <>
+      <div style={{ marginTop: "0", padding: "20px", backgroundColor: "black", }}>
+        <Home />
       </div>
-    </Router>
+    </>
   );
 };
 
