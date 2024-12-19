158c71a37b54e5ade4a6c9c097f6b116403ddf9e
KCCHONG1997 <94148987+KCCHONG1997@users.noreply.github.com>
2024-12-20 02:50:22 +0800

    Added Hashed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./view/Home";
+import DevilordPage from "./view/DevilordPage";
+import DevilordPageCursed from "./view/DevilordPageCursed";
 
 const App: React.FC = () => {
+  const isCursed = localStorage.getItem("Cursed") === "true";
+  // const [cursed,setCursed] = useState(isCursed);
+  useEffect(()=> {
+    
+  })
+
+  
   return (
-    <>
-      <div style={{ marginTop: "0", padding: "20px", backgroundColor: "black", }}>
-        <Home />
+      <div style={{ marginTop: "0", padding: "20px", backgroundColor: "black" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          {isCursed?<Route path="/devilord" element={<DevilordPageCursed />}/> :<Route path="/devilord" element={<DevilordPage />}/>}
+        </Routes>
       </div>
-    </>
   );
 };
 
---
+++ src/view/DevilordPageCursed.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const DevilordPageCursed: React.FC = () => {
+    return (
+        <>
+            <h2>Welcome Devilord</h2>
+            <div style={{ fontFamily: "Times New Roman, Times, serif", padding: "20px", color: "#780606", textAlign: "center" }}>
+                <h1>₩ɆⱠ₵Ø₥Ɇ ₮Ø ⱧɆⱠⱠ</h1>
+                <p style={{ maxWidth: "600px", margin: "0 auto", lineHeight: "1.8"}}>
+                    You know them... but you are mine now...
+                </p>
+            </div>
+
+        </>
+    );
+};
+
+export default DevilordPageCursed;
